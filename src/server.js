752df19efd4000c1d8577416dc60b7efb3c8de7b
Guilherme Moraes <guiferreiramoraes@outlook.com>
2023-07-17 20:47:09 -0300

    initial commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+// const path = require('path')
+
+// const db = require('./database')
+const routes = require('./routes/routes')
+
+const app = express()
+
+// db.connect()
+
+//habilita server para receber dados via post (forms)
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api', routes)
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
